0c48f7906aa7ba160facc357eab1a753b1e70fa4
Pierce Goulimis <piercemg2013@gmail.com>
2022-06-02 23:33:02 -0400

    Initial Commit

---
+++ DatingAppLogin/DatingAppLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatingAppLoginApp.swift
+//  DatingAppLogin
+//
+//  Created by Pierce Goulimis on 2022-06-02.
+//
+
+import SwiftUI
+
+@main
+struct DatingAppLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
